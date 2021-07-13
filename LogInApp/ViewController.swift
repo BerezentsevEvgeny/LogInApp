1fde111e828e3224bfd294457fec82d8625df2a4
Berezentsev Evgeny <jonn09@icloud.com>
2021-07-13 16:05:40 +0300

    Initial Commit

---
+++ LogInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by Евгений Березенцев on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
