78006a4f6e5dd8a0faf193ebd75002a9b0a7422b
Berezentsev Evgeny <jonn09@icloud.com>
2021-07-14 16:01:06 +0300

    Final

--- LogInApp/WelcomeViewController.swift
+++ LogInApp/WelcomeViewController.swift
@@ -13,9 +13,24 @@ class WelcomeViewController: UIViewController {
     
     var currentUser: String!
     
+    var gradient: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.type = .axial
+        gradient.colors = [
+            UIColor.red.cgColor,
+            UIColor.purple.cgColor,
+            UIColor.cyan.cgColor
+        ]
+        gradient.locations = [0, 0.25, 1]
+        return gradient
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         welcomeLabel.text = "Welcome,\(currentUser ?? "")!"
+        gradient.frame = view.bounds
+        view.layer.insertSublayer(gradient, at: 0)
     }
+    
 
 }
